ec3a1ea82a8e61cb2140d68f420f13916f9debcd
Özhan AKÇAY <ozhanakcay@aof.anadolu.edu.tr>
2021-01-23 23:55:38 +0300

    16 ocak Ders sonu

---
+++ KampIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KampIntro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string kategoriEtiketi = "Kategoriler";
+			int ogrenciSayisi = 32000;
+			double faizOrani = 1.45;
+			bool sistemeGirisYapmismi = false;
+			double dolarDun = 7.55;
+			double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+				Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+				Console.WriteLine("Artış butonu");
+            }
+
+            else
+            {
+				Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmismi == true)
+            {
+				Console.WriteLine("Kullanıcı Ayarları butonu");
+            }
+            else
+            {
+				Console.WriteLine("Giriş yap butonu");
+            }
+
+			Console.WriteLine(kategoriEtiketi);
+
+			
+		}
+	}
+}
